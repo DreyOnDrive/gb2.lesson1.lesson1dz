2116000d03a162f60675209f0e4b3fc57028428c
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-10-24 11:25:07 +0300

    Update Water.java

--- Water.java
+++ Water.java
@@ -1,1 +1,26 @@
+package gb2.lesson1.lesson1dz;
 
+public class Water extends Obstacle {
+
+    final private String name;
+    final private int length;
+
+    public Water(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return name + " длиной " + length +" м.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+}
