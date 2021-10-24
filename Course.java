683153399e57e1bcbdc18b44e59ef4f6a2991577
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-10-24 11:15:03 +0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,1 +1,41 @@
+package gb2.lesson1.lesson1dz;
 
+import java.util.Arrays;
+
+public class Course {
+
+    final private Obstacle[] obstacles;
+
+    public Course(Obstacle[] obstacles) {
+        this.obstacles = obstacles;
+    }
+
+    @Override
+    public String toString() {
+        return "Полоса препятствий состоит из следующих элементов: " + Arrays.toString(obstacles);
+    }
+
+    void printInformation() {
+        System.out.println(this.toString());
+    }
+
+    public void trying(Obstacle[] obstacles, Animal[] animals){
+        for (Animal animal : animals) {
+            for (Obstacle obstacle : obstacles) {
+                if (obstacle.getName() == "Яма") {
+                    animal.jump(obstacle.getLength());
+                }
+                if (obstacle.getName() == "Вода"){
+                    animal.swim(obstacle.getLength());
+                }
+                if (obstacle.getName() == "Стена"){
+                    animal.climb(obstacle.getLength());
+                }
+                if (obstacle.getName() == "Трек" ){
+                    animal.run(obstacle.getLength());
+                }
+            }
+            System.out.println();
+        }
+    }
+}
