a4fe9d02f03149ddce513b0d4c0a77e2752b1d06
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-10-24 11:24:02 +0300

    Update Track.java

--- Track.java
+++ Track.java
@@ -1,1 +1,26 @@
+package gb2.lesson1.lesson1dz;
 
+public class Track extends Obstacle {
+
+    final private String name;
+    final private int length;
+
+    public Track(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return name + " длиной " + length +" м.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+}
