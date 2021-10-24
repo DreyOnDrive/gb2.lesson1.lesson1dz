ccbdd4317e9e8b92f6255c87814a16f068476407
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-10-24 11:21:17 +0300

    Update Pit.java

--- Pit.java
+++ Pit.java
@@ -1,1 +1,25 @@
+package gb2.lesson1.lesson1dz;
 
+public class Pit extends Obstacle {
+
+    final private String name;
+    final  private int length;
+
+    public Pit(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return name + " длиной " + length +" м.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
