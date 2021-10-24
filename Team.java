f5439cb877a904c1c4ad4956d738b17b78d7882e
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-10-24 11:23:31 +0300

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,1 +1,33 @@
+package gb2.lesson1.lesson1dz;
 
+import java.util.Arrays;
+
+public class Team {
+
+    final private String name;
+    final private Animal [] animals;
+    private boolean win = true;
+
+    public Team(String name, Animal[] animals) {
+        this.name = name;
+        this.animals = animals;
+    }
+
+    void showResults(){
+        System.out.println("Объявление результатов");
+        for (Animal animal : animals) {
+            win = win & animal.isResult();
+        }
+        if (win == true){
+            System.out.println("Команда " + name + " прошла");
+        }else System.out.println("Команда " + name + " не прошла");
+    }
+
+    void printInformation (){
+        System.out.println(this.toString());
+    }
+    @Override
+    public String toString() {
+        return "Команда " + name + " и её участники: " + Arrays.toString(animals);
+    }
+}
